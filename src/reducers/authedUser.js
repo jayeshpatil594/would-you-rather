0491af25a2944601fd79f244372f85005f083412
Jayesh Patil <49228384+jayeshpatil594@users.noreply.github.com>
2020-05-08 16:35:35 +0530

    add avatars

--- src/reducers/authedUser.js
+++
@@ -1,8 +0,0 @@
-import { SET_AUTHED_USER } from '../actions/authedUser'
-
-export default function authedUser(state = null , action) {
-    if(action.type === SET_AUTHED_USER){
-        return action.id
-    }
-    return state
-}
