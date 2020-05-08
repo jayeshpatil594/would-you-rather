d04b1732d73d05ce690abc816021b9613dc24716
Jayesh Patil <49228384+jayeshpatil594@users.noreply.github.com>
2020-05-08 19:10:21 +0530

    data files

--- src/data/API.js
+++ src/data/API.js
@@ -1,23 +1,23 @@
 import {
-    _getUsers,
-    _getQuestions,
-    _saveQuestion,
-    _saveQuestionAnswer
+  _getUsers,
+  _getQuestions,
+  _saveQuestion,
+  _saveQuestionAnswer
 } from './_DATA'
 
 export function getInitialData() {
-    return Promise.all([_getUsers(), _getQuestions()]).then(
-        ([users, questions]) => ({
-            users,
-            questions
-        })
-    )
+  return Promise.all([_getUsers(), _getQuestions()]).then(
+    ([users, questions]) => ({
+      users,
+      questions
+    })
+  )
 }
 
-export function saveQuestion(info){
-    return _saveQuestion(info)
+export function saveQuestion(question) {
+  return _saveQuestion(question)
 }
 
-export function saveQuestionAnswer(info) {
-    return _saveQuestionAnswer(info)
+export function saveQuestionAnswer(authUser, qid, answer) {
+  return _saveQuestionAnswer({ authUser, qid, answer })
 }
