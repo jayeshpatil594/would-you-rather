f70be41fec7b2443aee68bb69f736dc96e9c4965
Jayesh Patil <49228384+jayeshpatil594@users.noreply.github.com>
2020-05-08 16:34:40 +0530

    update reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux'
-import authedUser from '../reducers/authedUser'
+import authUser from '../reducers/authUser'
 import questions from '../reducers/questions'
 import users from '../reducers/users'
 
 export default combineReducers({
-    authedUser,
-    questions,
-    users
+  authUser,
+  questions,
+  users
 })
